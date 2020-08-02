6f30cffbf1992d0a6d1df8071cece0dec160623c
Yue Tan <yuetan@cs.princeton.edu>
2020-08-02 16:37:20 -0400

    update profile to take hardware type as parameter

--- profile.py
+++ profile.py
@@ -1,4 +1,4 @@
-"""Ubuntu 16.04; kernel 4.15"""
+"""Ubuntu 16.04; kernel 4.15; Two connected nodes"""
 
 #
 # NOTE: This code was machine converted. An actual human would not
@@ -18,12 +18,37 @@ pc = portal.Context()
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
-# Node node
-node = request.RawPC('node')
-node.disk_image = 'urn:publicid:IDN+clemson.cloudlab.us+image+praxis-PG0:firecracker'
-node.hardware_type = 'c220g5'
-bs = node.Blockstore("bs", "/mydata")
-bs.size = "50GB"
+pc.defineParameter("phystype1",  "Optional physical type for node1",
+                   portal.ParameterType.STRING, "")
+pc.defineParameter("phystype2",  "Optional physical type for node2",
+                   portal.ParameterType.STRING, "")
+params = pc.bindParameters()
+
+# Create all the nodes.
+nodes = []
+for i in range(0, 2):
+    node = request.RawPC("node%d" % (i + 1))
+    if params.phystype != "":
+        node.hardware_type = params.phystype
+        pass
+    nodes.append(node)
+    pass
+
+# node.disk_image = 'urn:publicid:IDN+clemson.cloudlab.us+image+praxis-PG0:firecracker'
+nodes[0].disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+praxis-PG0:mongodb4.4-Ubuntu16'
+nodes[1].disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+praxis-PG0:mongodb4.4-Ubuntu16'
+
+# Create the link
+nodeA = nodes[0]
+nodeB = nodes[1]
+
+iface1 = nodeA.addInterface()
+iface2 = nodeB.addInterface()
+link = request.Link()
+link.addInterface(iface1)
+link.addInterface(iface2)
+link.best_effort = True
+# link.link_multiplexing = True
 
 # Print the generated rspec
 pc.printRequestRSpec(request)
