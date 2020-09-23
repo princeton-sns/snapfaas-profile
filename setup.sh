90cc348c9e812ebfe6e0e57c3cc0be9a13780a55
Yue Tan <yuetan@cs.princeton.edu>
2020-09-23 02:04:07 -0400

    update setup.sh to populate couchdb

--- setup.sh
+++ setup.sh
@@ -15,5 +15,24 @@ fi
 
 # node 2
 if [ $(uname -a | awk '{print $2}' | awk -F. '{print $1}') == 'node2' ]; then
-    .
+    COUCHDBURL='http://admin:admin@localhost:5984'
+    curl -X PUT $COUCHDBURL/reminders
+    curl -X PUT $COUCHDBURL/images
+    curl -X PUT $COUCHDBURL/audios
+    cd /local/repository/images
+    for fname in $(ls .); do
+        uuid=$(curl -H 'Content-Type: application/json' $COUCHDBURL/_uuids | awk -F\" '{print $4}')
+        extension=$(echo $fname | awk -F. '{print $2}')
+        curl -X PUT $COUCHDBURL/images/$uuid/$fname \
+            --data-binary @$fname \
+            -H 'Content-Type:image/'$extension
+    done
+    cd /local/repository/audios
+    for fname in $(ls .); do
+        uuid=$(curl -H 'Content-Type: application/json' $COUCHDBURL/_uuids | awk -F\" '{print $4}')
+        extension=$(echo $fname | awk -F. '{print $2}')
+        curl -X PUT $COUCHDBURL/images/$uuid/$fname \
+            --data-binary @$fname \
+            -H 'Content-Type:audio/'$extension
+    done
 fi
